626162123e1dd80991721809172a3af779492afc
Keith Welch <kwelch@mindspring.com>
2019-02-05 09:47:44 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,41 +12,19 @@
         
         var practitioner = "none";
         
-        $('#pract').html(JSON.stringify( smart ));
+        $('#pract').html( smart.tokenResponse.user );
+        $('#fhirid').html( smart.tokenResponse.patient);
         
         var patient = smart.patient;
         var pt = patient.read();
-/*        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
-*/
+
         $.when(pt, pr).fail(onError);
 
         $.when(pt, pr).done(function(patient) {
-          var gender = patient.gender;
-
-          var fname = '';
-          var lname = '';
-
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
-          }
 
           var p = defaultPatient();
           p.id = patient.id;
           p.mrn = patient.identifier[0].value;;
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
           p.pract = smart;
 
           ret.resolve(p);
@@ -65,10 +43,6 @@
     return {
       id: {value: ''},
       mrn: {value: ''},
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
       pracitioner: {value:''}
     };
   }
@@ -77,12 +51,8 @@
   window.drawVisualization = function(p) {
     $('#holder').show();
     $('#loading').hide();
-    $('#fhirid').html(p.id);
+//    $('#fhirid').html(p.id);
     $('#mrn').html(p.mrn)
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
     
   };
 
